9f4cae89c5542858b80be3e967fa58148c94deef
Macario <maca@aelita.io>
2022-07-07 05:44:03 +0200

    Convert to singular

---
+++ src/Inflect.elm
@@ -0,0 +1,170 @@
+module Inflect exposing (toPlural, toSingular)
+
+{-| String pluralization and singularization
+
+@docs toPlural, toSingular
+
+-}
+
+import Regex exposing (Regex)
+import String exposing (toUpper)
+import String.Extra exposing (toTitleCase)
+import Tuple exposing (pair)
+
+
+{-| Convert word to plural form.
+
+    toPlural "potato" == "potatoes"
+
+    toPlural "cat" == "cats"
+
+    toPlural "The Illustrated Man" == "The Illustrated Men"
+
+    toPlural "SHOUT" == "SHOUTS"
+
+-}
+toPlural : String -> String
+toPlural string =
+    Debug.todo "crash"
+
+
+{-| Convert word to singlular form.
+
+    toSingular "potatoes" == "potato"
+
+    toSingular "cats" == "cat"
+
+    toSingular "The Illustrated Men" == "The Illustrated Man"
+
+    toSingular "SHOUTS" == "SHOUT"
+
+-}
+toSingular : String -> String
+toSingular string =
+    List.foldl (replaceFoldFun string) Nothing rulesSingular
+        |> Maybe.withDefault string
+
+
+replaceFoldFun : String -> ( String, String ) -> Maybe String -> Maybe String
+replaceFoldFun string ( expression, replacement ) result =
+    case result of
+        Just _ ->
+            result
+
+        Nothing ->
+            Regex.find (parse (expression ++ "$")) string
+                |> List.head
+                |> Maybe.andThen
+                    (\{ submatches, match } ->
+                        case submatches of
+                            a :: (Just _) :: _ ->
+                                Just
+                                    (String.replace match
+                                        (Maybe.withDefault "" a ++ replacement)
+                                        string
+                                    )
+
+                            (Just s) :: _ ->
+                                Just
+                                    (String.replace match
+                                        (s ++ replacement)
+                                        string
+                                    )
+
+                            _ ->
+                                Nothing
+                    )
+
+
+rulesSingular : List ( String, String )
+rulesSingular =
+    List.map (\( s, p ) -> ( "(^|-|_|\\s)(" ++ p ++ ")", s )) irregulars
+        ++ List.concatMap
+            (\tuple ->
+                [ tuple
+                , Tuple.mapFirst titleizeExpression tuple
+                , Tuple.mapBoth toUpper toUpper tuple
+                ]
+            )
+            [ pair "(ox)en" ""
+            , pair "(movi|sho)es" "e"
+            , pair "(bus|fax|fox|alias|estatus|sex|census|circus)es" ""
+            , pair "(hiv)es" "e"
+            , pair "(ax)es" "is"
+            , pair "(alia|analy|antithe|oa|ba|diagno|ellip|hypothe|neme|neuro|parenthe|synop|the)ses" "sis"
+            , pair "(cris|test)es" "is"
+            , pair "(alumn|bacill|cact|foc|fung|hippopotam|loc|nucle|vir|octop|stimul|syllab)i" "us"
+            , pair "(corp)ora" "us"
+            , pair "(phenomen|criteri)a" "on"
+            , pair "(millenni|agend|dat|memorand|medi|aquari|bacteri|strat|curricul|addend|errat|ov|phyl|referend|symposi|stadi)a" "um"
+            , pair "(cal|lea|hoo|loa|scar|sel|thie|whar|wol|dwar|hal|el)ves" "f"
+            , pair "(kni|li|sa|wi)ves" "fe"
+            , pair "(appendi|matri)ces" "x"
+            , pair "(ap|cod|ind|vert|vort)ices" "ex"
+            , pair "(series|news|species|barracks|gallows|means|lux)" ""
+            , pair "(categor|quer|agenc|abilit)ies" "y"
+            , pair "(cap|sens|zombi|servic|cas)es" "e"
+            , pair "(flux|abyss|foss|search|switch|fix|box|process|address|wish|status|dash)es" ""
+            , pair "(.*o)es" ""
+            , pair "(.*i)i" "us"
+            , pair "(.*)men" "man"
+            , pair "(.*)people" "person"
+            , pair "(.*)ae" "a"
+            , pair "(.*)ux" "u"
+            , pair "(.*z)zes" ""
+            , pair "(.*)s" ""
+            ]
+
+
+irregulars : List ( String, String )
+irregulars =
+    List.concatMap
+        (\tuple ->
+            [ tuple
+            , Tuple.mapBoth toTitleCase toTitleCase tuple
+            , Tuple.mapBoth toUpper toUpper tuple
+            ]
+        )
+        [ pair "child" "children"
+        , pair "concerto" "concerti"
+        , pair "database" "databases"
+        , pair "die" "dice"
+        , pair "faeces" "feces"
+        , pair "faux pas" "faux pas"
+        , pair "fez" "fezes"
+        , pair "foot" "feet"
+        , pair "genus" "genera"
+        , pair "goose" "geese"
+        , pair "jeans" "jeans"
+        , pair "graffito" "graffiti"
+        , pair "libretto" "libretti"
+        , pair "louse" "lice"
+        , pair "man" "men"
+        , pair "person" "people"
+        , pair "mouse" "mice"
+        , pair "opus" "opera"
+        , pair "pants" "pants"
+        , pair "runner-up" "runners-up"
+        , pair "scissors" "scissors"
+        , pair "son-in-law" "sons-in-law"
+        , pair "tooth" "teeth"
+        ]
+
+
+titleizeExpression : String -> String
+titleizeExpression expression =
+    Regex.replace (parse "([(|])(\\w)")
+        (\{ submatches } ->
+            case submatches of
+                (Just a) :: (Just s) :: _ ->
+                    a ++ toTitleCase s
+
+                _ ->
+                    ""
+        )
+        expression
+
+
+parse : String -> Regex
+parse =
+    Regex.fromString >> Maybe.withDefault Regex.never
---
+++ tests/Tests.elm
@@ -0,0 +1,306 @@
+module Tests exposing (..)
+
+import Expect exposing (Expectation)
+import Inflect
+import String exposing (toUpper)
+import String.Extra exposing (toTitleCase)
+import Test exposing (..)
+import Tuple exposing (pair)
+
+
+examples : List ( String, String )
+examples =
+    [ -- (-f, -fe)
+      pair "calf" "calves"
+    , pair "dwarf" "dwarves"
+    , pair "elf" "elves"
+    , pair "fez" "fezes"
+    , pair "half" "halves"
+    , pair "hive" "hives"
+    , pair "hoof" "hooves"
+    , pair "knife" "knives"
+    , pair "leaf" "leaves"
+    , pair "life" "lives"
+    , pair "loaf" "loaves"
+    , pair "proof" "proofs"
+    , pair "quiz" "quizzes"
+    , pair "roof" "roofs"
+    , pair "safe" "saves"
+    , pair "scarf" "scarves"
+    , pair "self" "selves"
+    , pair "shelf" "shelves"
+    , pair "thief" "thieves"
+    , pair "wharf" "wharves"
+    , pair "wife" "wives"
+    , pair "wolf" "wolves"
+
+    --
+    , pair "ability" "abilities"
+    , pair "address" "addresses"
+    , pair "agency" "agencies"
+    , pair "box" "boxes"
+    , pair "cape" "capes"
+    , pair "case" "cases"
+    , pair "category" "categories"
+    , pair "dash" "dashes"
+    , pair "fix" "fixes"
+    , pair "flux" "fluxes"
+    , pair "movie" "movies"
+    , pair "process" "processes"
+    , pair "query" "queries"
+    , pair "search" "searches"
+    , pair "shoe" "shoes"
+    , pair "switch" "switches"
+    , pair "wish" "wishes"
+
+    -- (-o)
+    , pair "potato" "potatoes"
+    , pair "tomato" "tomatoes"
+    , pair "hero" "heroes"
+    , pair "torpedo" "torpedoes"
+    , pair "veto" "vetoes"
+    , pair "oasis" "oases"
+    , pair "buffalo" "buffaloes"
+
+    -- (-us) (-i)
+    , pair "alumnus" "alumni"
+    , pair "bacillus" "bacilli"
+    , pair "cactus" "cacti"
+    , pair "corpus" "corpora"
+    , pair "focus" "foci"
+    , pair "fungus" "fungi"
+    , pair "genus" "genera"
+    , pair "graffito" "graffiti"
+    , pair "hippopotamus" "hippopotami"
+    , pair "locus" "loci"
+    , pair "nucleus" "nuclei"
+    , pair "octopus" "octopi"
+    , pair "opus" "opera"
+    , pair "radius" "radii"
+    , pair "stimulus" "stimuli"
+    , pair "syllabus" "syllabi"
+    , pair "virus" "viri"
+
+    -- (-es)
+    , pair "alias" "aliases"
+    , pair "analysis" "analyses"
+    , pair "antithesis" "antitheses"
+    , pair "apex" "apices"
+    , pair "axis" "axes"
+    , pair "basis" "bases"
+    , pair "bus" "buses"
+    , pair "census" "censuses"
+    , pair "circus" "circuses"
+    , pair "codex" "codices"
+    , pair "crisis" "crises"
+    , pair "database" "databases"
+    , pair "diagnosis" "diagnoses"
+    , pair "ellipsis" "ellipses"
+    , pair "estatus" "estatuses"
+    , pair "fax" "faxes"
+    , pair "fox" "foxes"
+    , pair "hypothesis" "hypotheses"
+    , pair "nemesis" "nemeses"
+    , pair "neurosis" "neuroses"
+    , pair "parenthesis" "parentheses"
+    , pair "sense" "senses"
+    , pair "status" "statuses"
+    , pair "synopsis" "synopses"
+    , pair "testis" "testes"
+    , pair "thesis" "theses"
+
+    -- (-ss)
+    , pair "abyss" "abysses"
+    , pair "foss" "fosses"
+
+    -- (-on)
+    , pair "phenomenon" "phenomena"
+    , pair "criterion" "criteria"
+
+    -- (-ae)
+    , pair "alumna" "alumnae"
+    , pair "antenna" "antennae"
+    , pair "larva" "larvae"
+    , pair "minutia" "minutiae"
+    , pair "nebula" "nebulae"
+    , pair "vertebra" "vertebrae"
+    , pair "vita" "vitae"
+    , pair "formula" "formulae"
+
+    -- (-um)
+    , pair "addendum" "addenda"
+    , pair "agendum" "agenda"
+    , pair "aquarium" "aquaria"
+    , pair "bacterium" "bacteria"
+    , pair "curriculum" "curricula"
+    , pair "datum" "data"
+    , pair "erratum" "errata"
+    , pair "medium" "media"
+    , pair "memorandum" "memoranda"
+    , pair "millennium" "millennia"
+    , pair "ovum" "ova"
+    , pair "phylum" "phyla"
+    , pair "referendum" "referenda"
+    , pair "stadium" "stadia"
+    , pair "stratum" "strata"
+    , pair "symposium" "symposia"
+
+    -- (-x)
+    , pair "appendix" "appendices"
+    , pair "index" "indices"
+    , pair "matrix" "matrices"
+    , pair "vertex" "vertices"
+    , pair "vortex" "vortices"
+
+    -- vowel change
+    , pair "foot" "feet"
+    , pair "tooth" "teeth"
+    , pair "goose" "geese"
+    , pair "man" "men"
+    , pair "woman" "women"
+
+    -- very irregular
+    , pair "louse" "lice"
+    , pair "child" "children"
+    , pair "die" "dice"
+    , pair "mouse" "mice"
+    , pair "ox" "oxen"
+    , pair "person" "people"
+    , pair "faeces" "feces"
+    , pair "salesperson" "salespeople"
+    , pair "spokesman" "spokesmen"
+
+    -- uncountable
+    , pair "advice" "advice"
+    , pair "aircraft" "aircraft"
+    , pair "barracks" "barracks"
+    , pair "bison" "bison"
+    , pair "buffalo" "buffalo"
+    , pair "deer" "deer"
+    , pair "duck" "duck"
+    , pair "equipment" "equipment"
+    , pair "fish" "fish"
+    , pair "gallows" "gallows"
+    , pair "grouse" "grouse"
+    , pair "hovercraft" "hovercraft"
+    , pair "information" "information"
+    , pair "jeans" "jeans"
+    , pair "lux" "lux"
+    , pair "means" "means"
+    , pair "miniseries" "miniseries"
+    , pair "moose" "moose"
+    , pair "news" "news"
+    , pair "offspring" "offspring"
+    , pair "pants" "pants"
+    , pair "pike" "pike"
+    , pair "police" "police"
+    , pair "rice" "rice"
+    , pair "salmon" "salmon"
+    , pair "scissors" "scissors"
+    , pair "series" "series"
+    , pair "sheep" "sheep"
+    , pair "shrimp" "shrimp"
+    , pair "spacecraft" "spacecraft"
+    , pair "species" "species"
+    , pair "squid" "squid"
+    , pair "swine" "swine"
+    , pair "trout" "trout"
+    , pair "tuna" "tuna"
+
+    -- regular
+    , pair "archive" "archives"
+    , pair "day" "days"
+    , pair "document" "documents"
+    , pair "edge" "edges"
+    , pair "experience" "experiences"
+    , pair "foo" "foos"
+    , pair "horse" "horses"
+    , pair "house" "houses"
+    , pair "move" "moves"
+    , pair "perspective" "perspectives"
+    , pair "photo" "photos"
+    , pair "portfolio" "portfolios"
+    , pair "prize" "prizes"
+    , pair "service" "services"
+    , pair "shout" "shouts"
+    , pair "slice" "slices"
+    , pair "stack" "stacks"
+    , pair "taxi" "taxis"
+    , pair "underscore" "underscores"
+    , pair "zombie" "zombies"
+
+    -- other
+    , pair "beau" "beaux"
+    , pair "tableau" "tableaux"
+    , pair "bureau" "bureaux"
+    , pair "château" "châteaux"
+    , pair "faux pas" "faux pas"
+    , pair "concerto" "concerti"
+    , pair "libretto" "libretti"
+    , pair "comment" "comments"
+    , pair "foobar" "foobars"
+    , pair "newsletter" "newsletters"
+
+    -- compound
+    , pair "runner-up" "runners-up"
+    , pair "son-in-law" "sons-in-law"
+
+    -- with adjective
+    , pair "quick fox" "quick foxes"
+    , pair "the illustrated man" "the illustrated men"
+
+    -- underscore, dash or symbols
+    , pair "my_analysis" "my_analyses"
+    , pair "node_child" "node_children"
+    , pair "old-news" "old-news"
+    , pair "matrix_fu" "matrix_fus"
+    , pair "status_code" "status_codes"
+    , pair "|ice" "|ices"
+    , pair "|ouse" "|ouses"
+    ]
+
+
+suite : Test
+suite =
+    describe "Inflect"
+        [ describe "toSingular"
+            [ test "lower case string" <|
+                \_ ->
+                    Expect.all
+                        (List.map
+                            (\( singular, plural ) ->
+                                always <|
+                                    Expect.equal singular
+                                        (Inflect.toSingular plural)
+                            )
+                            examples
+                        )
+                        ()
+            , test "title case string" <|
+                \_ ->
+                    Expect.all
+                        (List.map
+                            (\( singular, plural ) ->
+                                always <|
+                                    Expect.equal (toTitleCase singular)
+                                        (Inflect.toSingular
+                                            (toTitleCase plural)
+                                        )
+                            )
+                            examples
+                        )
+                        ()
+            , test "upper case string" <|
+                \_ ->
+                    Expect.all
+                        (List.map
+                            (\( singular, plural ) ->
+                                always <|
+                                    Expect.equal (toUpper singular)
+                                        (Inflect.toSingular (toUpper plural))
+                            )
+                            examples
+                        )
+                        ()
+            ]
+        ]
